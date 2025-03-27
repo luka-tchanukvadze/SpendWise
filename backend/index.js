825a22424dc93ac981cc8ae5d9db65c8642f6430
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-27 17:00:25 +0400

    adding user and transaction schema

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
@@ -9,6 +10,7 @@ import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHt
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+dotenv.config();
 const app = express();
 
 const httpServer = http.createServer(app);
---
+++ backend/models/user.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const usesrSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", usesrSchema);
+
+export default User;
