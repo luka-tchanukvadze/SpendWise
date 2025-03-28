3745d2cd61a8de5e2e1608d6c5c57bb4e5586e23
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-28 22:36:07 +0400

    hashed password

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -1,4 +1,6 @@
 import { users } from "../dummyData/data.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 const userResolver = {
   Mutation: {
@@ -9,6 +11,14 @@ const userResolver = {
         if (!username || !name || !password || !gender) {
           throw new Error("All fields are required");
         }
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+          throw new Error("User already exists");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
       } catch (error) {}
     },
   },
