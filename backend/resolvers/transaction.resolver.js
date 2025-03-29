fd96c70408d19dbdebd0de0dd55360ca081af41d
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-29 11:10:39 +0400

    transactions resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,21 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Query: {},
+  Query: {
+    transactions: async (_, __dirname, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unsauthorized");
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+
+        return transactions;
+      } catch (err) {
+        console.error("Error getting transactions", err);
+        throw new Error("Error getting transactions");
+      }
+    },
+  },
   Mutation: {},
 };
 
