526df96c375644d12428473f4196bdc2959e6570
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-03-31 12:29:40 +0400

    transaciton skelaton

--- frontend/src/pages/TransactionPage.jsx
+++ frontend/src/pages/TransactionPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import TransactionFormSkeleton from "../components/skeletons/TransactionFormSkeleton";
 
 const TransactionPage = () => {
   const [formData, setFormData] = useState({
