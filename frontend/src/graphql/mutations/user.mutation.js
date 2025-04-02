c5b471aae661d5f8f5c1e94d375f015df2598c57
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-02 20:48:36 +0400

    Login mutation

--- frontend/src/graphql/mutations/user.mutation.js
+++ frontend/src/graphql/mutations/user.mutation.js
@@ -10,6 +10,16 @@ export const SIGN_UP = gql`
   }
 `;
 
+export const LOGIN = gql`
+  mutation Login($input: LoginInput!) {
+    login(input: $input) {
+      _id
+      name
+      username
+    }
+  }
+`;
+
 export const LOGOUT = gql`
   mutation Logout {
     logout {
