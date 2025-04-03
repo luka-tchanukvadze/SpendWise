e0ff137989d299e3d7f272379d5c6c560d9ad2dd
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-03 20:49:02 +0400

    create transaction

--- frontend/src/components/TransactionForm.jsx
+++ frontend/src/components/TransactionForm.jsx
@@ -1,4 +1,10 @@
+import { useMutation } from "@apollo/client";
+import { toast } from "react-hot-toast";
+
 const TransactionForm = () => {
+  const [createTransaction, { loading, error }] =
+    useMutation(CREATE_TRANSACTION);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -12,7 +18,15 @@ const TransactionForm = () => {
       location: formData.get("location"),
       date: formData.get("date"),
     };
-    console.log("transactionData", transactionData);
+
+    try {
+      await createTransaction({ variables: { input: transactionData } });
+
+      form.reset();
+      toast.success("Transaction created successfully");
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   return (
@@ -159,8 +173,9 @@ const TransactionForm = () => {
           from-pink-500 to-pink-500 hover:from-pink-600 hover:to-pink-600
 						disabled:opacity-70 disabled:cursor-not-allowed"
         type="submit"
+        disabled={loading}
       >
-        Add Transaction
+        {loading ? "Loading..." : "Add Transaction"}
       </button>
     </form>
   );
