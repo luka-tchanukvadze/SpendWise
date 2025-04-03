09371e295022c118ed93076065e3c42dee605d4c
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-03 20:56:54 +0400

    fixing transaction mutation

--- frontend/src/graphql/mutations/transaction.mutation.js
+++ frontend/src/graphql/mutations/transaction.mutation.js
@@ -29,9 +29,9 @@ export const UPDATE_TRANSACTION = gql`
 `;
 
 export const DELETE_TRANSACTION = gql`
-  mutation deleteTransaction($transactionId: ID!){
-    deleteTransaction($transactionId: $$transactionId){
-      _id 
+  mutation deleteTransaction($transactionId: ID!) {
+    deleteTransaction(transactionId: $transactionId) {
+      _id
     }
   }
 `;
