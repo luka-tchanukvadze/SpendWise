be35ed65150168abd3c8333d765c90a67333a61f
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-04 12:16:33 +0400

    usying transaction data for cards

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -6,19 +6,16 @@ const Cards = () => {
   const { data, loading, error } = useQuery(GET_TRANSACTIONS);
 
   if (error) return <p>Error: {error.message}</p>;
-  if (loading) return <p>Loading...</p>;
 
   console.log("data", data);
   return (
     <div className="w-full px-10 min-h-[40vh]">
       <p className="text-5xl font-bold text-center my-10">History</p>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20">
-        <Card cardType={"saving"} />
-        <Card cardType={"expense"} />
-        <Card cardType={"investment"} />
-        <Card cardType={"investment"} />
-        <Card cardType={"saving"} />
-        <Card cardType={"expense"} />
+        {!loading &&
+          data.transactions.map((transaction) => (
+            <Card key={transaction._id} transaction={transaction} />
+          ))}
       </div>
     </div>
   );
