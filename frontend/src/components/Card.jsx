c7e7e779ff8ee32d93d710e0cd48bd999a91cacc
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-04-06 21:36:36 +0400

    adding refetch after deleting a transaction

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -23,7 +23,9 @@ const Card = ({ transaction }) => {
 
   description = description[0]?.toUpperCase() + description.slice(1);
   category = category[0]?.toUpperCase() + category.slice(1);
-  const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION);
+  const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
+    refetchQueries: ["GetTransactions"],
+  });
 
   const formattedDate = formatDate(date);
 
